34816db6ce53105e5fa964787918123aec9c0389
bihengcheng-gif <bihengcheng@gmail.com>
2026-04-18 10:47:34 +0800

    Update chat.ts

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -98,7 +98,8 @@ export interface ChatSession {
 export const DEFAULT_TOPIC = Locale.Store.DefaultTopic;
 export const BOT_HELLO: ChatMessage = createMessage({
   role: "assistant",
-  content: (typeof window !== 'undefined' && window.localStorage.getItem('access-control')?.includes('20061201')) ? "我是你的专属Ai代理，有什么可以帮您吗？" : Locale.Store.BotHello,
+  content: (typeof window !== 'undefined' && window.localStorage.getItem('access-control')?.includes('20061201')) ? "你好，有什么可以帮您吗？" : Locale.Store.BotHello,
+});
 
 function createEmptySession(): ChatSession {
   return {
